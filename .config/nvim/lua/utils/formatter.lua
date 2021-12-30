68f02d49ebf8b9f661934995cab42b047015d914
Nameer Metori <nameer6g6@gmail.com>
2021-12-31 00:46:00 +0300

    commented out formatter config temporary

--- .config/nvim/lua/utils/formatter.lua
+++ .config/nvim/lua/utils/formatter.lua
@@ -1,6 +1,6 @@
-vim.api.nvim_exec([[
-  augroup FormatAutogroup
-    autocmd!
-    autocmd BufWritePost *.js,*.rs,*.lua FormatWrite
-  augroup END
-  ]], true)
+-- vim.api.nvim_exec([[
+--   augroup FormatAutogroup
+--     autocmd!
+--     autocmd BufWritePost *.js,*.rs,*.lua FormatWrite
+--   augroup END
+--   ]], true)
