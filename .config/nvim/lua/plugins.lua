4cef7279a56e7495096ae4ab0f738af093b53832
Nameer Metori <nameer6g6@gmail.com>
2021-12-31 00:44:46 +0300

    removed chunck of undeeded code, added new plugins

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -273,6 +273,7 @@ require "telescope"
   use "hrsh7th/cmp-buffer" -- buffer completions
   use "hrsh7th/cmp-path" -- path completions
   use "hrsh7th/cmp-cmdline" -- cmdline completions
+  use "quangnguyen30192/cmp-nvim-tags"
   use 'saadparwaiz1/cmp_luasnip' -- Snippets source for nvim-cmp
   use 'ray-x/cmp-treesitter'
   use 'p00f/nvim-ts-rainbow'
@@ -280,6 +281,7 @@ require "telescope"
   use 'hrsh7th/cmp-calc'
   use 'hrsh7th/cmp-emoji'
   use {'f3fora/cmp-spell', opt = true, ft = {'markdown', 'text'}}
+  use 'onsails/lspkind-nvim'
 
 
   -- snippets
@@ -306,57 +308,3 @@ require "telescope"
     require('packer').sync()
   end
 end)
-
-
--- -- Lazy loading:
--- -- Load on specific commands
--- use {'tpope/vim-dispatch', opt = true, cmd = {'Dispatch', 'Make', 'Focus', 'Start'}}
-
--- -- Load on an autocommand event
--- use {'andymass/vim-matchup', event = 'VimEnter'}
-
--- -- Plugins can have dependencies on other plugins
--- use {
---   'haorenW1025/completion-nvim',
---   opt = true,
---   requires = {{'hrsh7th/vim-vsnip', opt = true}, {'hrsh7th/vim-vsnip-integ', opt = true}}
--- }
-
--- -- Plugins can also depend on rocks from luarocks.org:
--- use {
---   'my/supercoolplugin',
---   rocks = {'lpeg', {'lua-cjson', version = '2.1.0'}}
--- }
-
--- -- You can specify rocks in isolation
--- use_rocks 'penlight'
--- use_rocks {'lua-resty-http', 'lpeg'}
-
--- -- Local plugins can be included
--- use '~/projects/personal/hover.nvim'
-
--- -- Plugins can have post-install/update hooks
-
--- -- Post-install/update hook with neovim command
--- use { 'nvim-treesitter/nvim-treesitter', run = ':TSUpdate' }
-
--- -- Post-install/update hook with call of vimscript function with argument
--- use { 'glacambre/firenvim', run = function() vim.fn['firenvim#install'](0) end }
-
--- -- Use specific branch, dependency and run lua file after load
--- use {
---   'glepnir/galaxyline.nvim', branch = 'main', config = function() require'statusline' end,
---   requires = {'kyazdani42/nvim-web-devicons'}
--- }
-
--- -- Use dependency and run lua function after load
--- use {
---   'lewis6991/gitsigns.nvim', requires = { 'nvim-lua/plenary.nvim' },
---   config = function() require('gitsigns').setup() end
--- }
-
--- -- You can specify multiple plugins in a single call
--- use {'tjdevries/colorbuddy.vim', {'nvim-treesitter/nvim-treesitter', opt = true}}
-
--- -- You can alias plugin names
--- use {'dracula/vim', as = 'dracula'}
