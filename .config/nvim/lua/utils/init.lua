c7d7694d8f28a858da4a9fe2905a91970383ea5e
Nameer Metori <nameer6g6@gmail.com>
2022-01-09 15:49:54 +0300

    removed some imports and added due_nvim configs

--- .config/nvim/lua/utils/init.lua
+++ .config/nvim/lua/utils/init.lua
@@ -1,18 +1,18 @@
-require('utils.options')
-require('utils.keymap')
-require('utils.treesitter')
-require('utils.cmp')
-require('utils.colorscheme')
-require('utils.blank_line')
-require('utils.commands')
-require('utils.auto_pairs')
-require('utils.git_signs')
-require('utils.numb')
-require('utils.formatter')
-require('utils.zen_mode')
-require('utils.twilight')
--- require('utils.nvim_tree');
-require('utils.marks')
+require("utils.options")
+-- require("utils.keymap")
+-- require("utils.treesitter")
+-- require("utils.cmp")
+require("utils.colorscheme")
+require("utils.blank_line")
+require("utils.commands")
+require("utils.auto_pairs")
+require("utils.git_signs")
+require("utils.numb")
+require("utils.formatter")
+require("utils.zen_mode")
+require("utils.twilight")
+require("utils.marks")
+require("utils.tagbar")
 
 
 -- WARNING: need checking
@@ -22,32 +22,32 @@ vim.cmd("hi diffRemoved ctermfg=red")
 
 -- WARNING: reserved and need to be worked later
 -- Keep Eye
--- let g:keepeye_features = ['bell', 'notification', 'statusline']
--- " let g:keepeye_features = ['bell', 'notification']
+-- let g:keepeye_features = ["bell", "notification", "statusline"]
+-- " let g:keepeye_features = ["bell", "notification"]
 -- let g:keepeye_autostart = 1
 -- let g:keepeye_timer = 900
--- let g:keepeye_message = 'SAVE YOUR EYES, DRINK WATER, TAKE A BREAK'
+-- let g:keepeye_message = "SAVE YOUR EYES, DRINK WATER, TAKE A BREAK"
 
 -- Last place config
-require'nvim-lastplace'.setup {
+require"nvim-lastplace".setup {
     -- lastplace_ignore_buftype = {"quickfix", "nofile", "help"},
     -- lastplace_ignore_filetype = {"gitcommit", "gitrebase", "svn", "hgcommit"},
     -- lastplace_open_folds = true
 }
 
 -- Due time config
-require('due_nvim').setup {
-  prescript = 'due: ',           -- prescript to due data
-  prescript_hi = 'Comment',      -- highlight group of it
-  due_hi = 'String',             -- highlight group of the data itself
-  ft = '*.md',                   -- filename template to apply aucmds :)
-  today = 'TODAY',               -- text for today's due
-  today_hi = 'Character',        -- highlight group of today's due
-  overdue = 'OVERDUE',           -- text for overdued
-  overdue_hi = 'Error',          -- highlight group of overdued
-  date_hi = 'Conceal',           -- highlight group of date string
-  pattern_start = '<',           -- start for a date string pattern
-  pattern_end = '>',             -- end for a date string pattern
+require("due_nvim").setup {
+  prescript = "due: ",           -- prescript to due data
+  prescript_hi = "Comment",      -- highlight group of it
+  due_hi = "String",             -- highlight group of the data itself
+  ft = "*.md",                   -- filename template to apply aucmds :)
+  today = "TODAY",               -- text for today"s due
+  today_hi = "Character",        -- highlight group of today"s due
+  overdue = "OVERDUE",           -- text for overdued
+  overdue_hi = "Error",          -- highlight group of overdued
+  date_hi = "Conceal",           -- highlight group of date string
+  pattern_start = "<",           -- start for a date string pattern
+  pattern_end = ">",             -- end for a date string pattern
   use_clock_time = false,        -- allow due.nvim to calculate hours, minutes, and seconds
   default_due_time = "midnight", -- if use_clock_time == true, calculate time until option on specified date.
                                  --   Accepts "midnight", for 23:59:59, or noon, for 12:00:00
