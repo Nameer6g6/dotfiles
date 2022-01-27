7b167cb67deedfa7436ef81fdef551c4bbccaf9d
Nameer Metori <nameer6g6@gmail.com>
2022-01-27 10:06:56 +0300

    Moved FTerm and Todo-comments configs to plugins since they are simple configs, changed import names since it clash with plugin name

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -2,8 +2,8 @@ require "plugins"
 require "notify_configs"
 require "dap_configs"
 require "neorg_configs"
-require "cmp_configs"
 require "utils"
+require "cmp_configs"
 require "telescope"
 require "lsp"
 require "langs"
@@ -11,25 +11,6 @@ require "status_line"
 -- require "nvim_tree"
 require "treesitter"
 require "keymap"
-
-
--- NOTE: Todo configs
- require("todo-comments").setup {
-  -- your configuration comes here
-  -- or leave it empty to use the default settings
-  -- refer to the configuration section below
-}
-
--- NOTE: FTerm configs
-require'FTerm'.setup {
-    border = 'double',
-    dimensions  = {
-        height = 0.9, -- Height of the terminal window
-        width = 0.9, -- Width of the terminal window
-        -- x = 0.5, -- X axis of the terminal window
-        -- y = 0.5, -- Y axis of the terminal window
-    },
-}
 
 -- NOTE: Config for calendar
 -- Calendar.vim integrating with goolge
