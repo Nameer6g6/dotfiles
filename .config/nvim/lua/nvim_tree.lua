d26073d9b5d73eed8a4b8e2a4b1122e32c0f4ebb
Nameer Metori <nameer6g6@gmail.com>
2022-09-02 14:48:26 +0200

    adjusted not resizing window when open different file

--- .config/nvim/lua/nvim_tree.lua
+++ .config/nvim/lua/nvim_tree.lua
@@ -22,7 +22,7 @@ require("nvim-tree").setup {
     height = 30,
     hide_root_folder = false,
     side = "left",
-    preserve_window_proportions = false,
+    preserve_window_proportions = true,
     number = false,
     relativenumber = false,
     signcolumn = "yes",
@@ -140,7 +140,7 @@ require("nvim-tree").setup {
     },
     open_file = {
       quit_on_open = false,
-      resize_window = true,
+      resize_window = false,
       window_picker = {
         enable = true,
         chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890",
