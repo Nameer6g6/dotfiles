04e9566dc46b6013e2c9817a205e345a99f30607
Nameer Metori <nameer6g6@gmail.com>
2022-10-16 23:37:24 +0200

    removed old zen file

--- .config/nvim/lua/lsp/settings/haskell.lua
+++ .config/nvim/lua/lsp/settings/haskell.lua
@@ -4,13 +4,13 @@ local on_attach = require("lsp.handlers").on_attach
 local capabilities = require("lsp.handlers").capabilities
 
 lspconfig.hls.setup {
-         on_attach = on_attach,
-         capabilities = capabilities,
-         single_file_support = true,
-         settings = {
-             haskell = {
-                 hlintOn = false,
-             }
-         }
- }
+  on_attach = on_attach,
+  capabilities = capabilities,
+  single_file_support = true,
+  settings = {
+    haskell = {
+      hlintOn = false,
+    }
+  }
+}
 
