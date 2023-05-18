19cc0b71830c8799d253152bb67a2513e6ad7c3d
Nameer Metori <nameer6g6@gmail.com>
2023-05-18 18:43:01 +0200

    added mason

--- .config/nvim/lua/plugins.lua
+++ .config/nvim/lua/plugins.lua
@@ -194,25 +194,7 @@ packer.startup(function(use)
   use "Pocco81/DAPInstall.nvim"
 
   -- Lsp
-  -- use {
-  --   "VonHeikemen/lsp-zero.nvim",
-  --   branch = "v2.x",
-  --   requires = {
-  --     -- LSP Support
-  --     { "neovim/nvim-lspconfig" }, -- Required
-  --     { -- Optional
-  --       "williamboman/mason.nvim",
-  --       run = function()
-  --         pcall(vim.cmd, "MasonUpdate")
-  --       end,
-  --     },
-  --     { "williamboman/mason-lspconfig.nvim" }, -- Optional
-  --     -- Autocompletion
-  --     -- { "hrsh7th/nvim-cmp" }, -- Required
-  --     -- { "hrsh7th/cmp-nvim-lsp" }, -- Required
-  --     { "L3MON4D3/LuaSnip" }, -- Required
-  --   },
-  -- }
+  use { "williamboman/mason.nvim", run = ":MasonUpdate" }
   use "neovim/nvim-lspconfig"
   use "nvim-lua/lsp_extensions.nvim"
   use "ray-x/lsp_signature.nvim"
