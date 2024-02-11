261ceff15f253e6b14f5ae443140b5aeb8aa2485
Nameer Metori <nameer6g6@gmail.com>
2024-02-11 17:13:28 +0100

    Added haskell configs

--- .config/nvim/lua/plugins/haskell.lua
+++ .config/nvim/lua/plugins/haskell.lua
@@ -1,17 +1,21 @@
 return {
   {
-    "mrcjkb/haskell-tools.nvim",
-  },
-  {
     "monkoose/fzf-hoogle.vim",
-    ft = "haskell",
+    ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
   },
   {
     "Twinside/vim-haskellFold",
     enabled = false,
+    ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
   },
   {
     "enomsg/vim-haskellConcealPlus",
     enabled = false,
+    ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
+  },
+  {
+    "mrcjkb/haskell-tools.nvim",
+    version = "^3", -- Recommended
+    ft = { "haskell", "lhaskell", "cabal", "cabalproject" },
   },
 }
